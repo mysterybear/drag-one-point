202259955b0ddce49ef60b7467f577149119032d
Tom <tom@bearjam.dev>
2020-03-27 17:39:14 +0000

    DragOnePoint.js

---
+++ components/DragOnePoint.js
@@ -0,0 +1,42 @@
+import { scaleLinear } from 'd3';
+import { motion } from 'framer-motion';
+import produce from 'immer';
+import React, { useRef, useState } from 'react';
+
+const DragOnePoint = () => {
+  const
+    svgRef = useRef(),
+
+    xScale = scaleLinear().domain([-5, 5]).range([0, 800]),
+    yScale = scaleLinear().domain([-5, 5]).range([800, 0]),
+
+    [ , width ] =  xScale.range(),
+    [ height ] = yScale.range(),
+
+    [circle, setCircle] = useState({ cx: 300, cy: 200 })
+    ;
+
+  return (
+    <svg className="bg-blue-300 touch-none" viewBox={`0 0 ${width} ${height}`} ref={svgRef}>
+      <motion.circle
+        onPan={(e, info) => {
+          setCircle(produce(draft => {
+            let point = svgRef.current.createSVGPoint()
+            point.x = info.point.x
+            point.y = info.point.y
+            point = point.matrixTransform(svgRef.current.getScreenCTM().inverse())
+            draft.cx = point.x
+            draft.cy = point.y
+          }));
+        }}
+        cx={circle.cx}
+        cy={circle.cy}
+        r={100}
+        stroke="gray"
+        fill="pink"
+      />
+    </svg>
+  )
+}
+
+export default DragOnePoint;
